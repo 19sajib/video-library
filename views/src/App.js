90e7ec9e201aa1ab3d8c2536b70f862252fb8084
Sajib <contact2sajib@gmail.com>
2022-09-26 16:26:26 +0600

    initial frontend

---
+++ views/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <div>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" exact element={<Home />} />
+          </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
