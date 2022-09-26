05a5281bfd8e6c9dd36ee097f092c9b24bfdc624
Sajib <contact2sajib@gmail.com>
2022-09-26 15:12:38 +0600

    initial backend

---
+++ index.js
@@ -0,0 +1,33 @@
+const cors = require('cors')
+const dotenv = require('dotenv')
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
+
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({limit: '30mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(cors())
+
+// Server Setting
+
+const PORT = process.env.PORT || 9090;
+const server = app.listen(PORT, ()=> console.log(`Server is up and running on port: ${PORT}`));
+
+// DB Setting
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+   }, (err) => {
+    if(err) return console.log(err);
+    console.log('MongoDB connected...')
+   })
+
+app.use('/', (req,res)=> {
+res.send('Running Fine!')
+})
