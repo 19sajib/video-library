76f0cbf2417b81dc41a679cd00f6504501eca8d3
Sajib <contact2sajib@gmail.com>
2022-09-27 11:00:51 +0600

    redux store setup

--- views/src/App.js
+++ views/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
+// import Auth from './pages/Auth'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <BrowserRouter>
           <Routes>
               <Route path="/" exact element={<Home />} />
+              {/* <Route path="/auth" element={<Auth />} /> */}
           </Routes>
       </BrowserRouter>
     </div>
